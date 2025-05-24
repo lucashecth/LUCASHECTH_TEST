11153e7bc69c202f8ba108b28b46c4ebb8179449
Lucas Hecth <lucas.hecth@gmail.com>
2025-05-24 19:29:15 -0300

    FEAT: Added the inventory item swappable

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -5,20 +5,17 @@ using UnityEngine.UI;
 public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image image;
-    public Transform parentToReturnTo;
+    public Transform parentAfterDrag;
     public void OnBeginDrag(PointerEventData eventData)
-
     {
-        // Handle the beginning of the drag
         image.raycastTarget = false; // Disable raycast target to allow dragging
-        parentToReturnTo = transform.parent; // Store the parent to return to
+        parentAfterDrag = transform.parent; // Store the parent to return to
         transform.SetParent(transform.root); // Set the parent to the root to allow free movement
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         // Handle the dragging
-        Debug.Log("Dragging");
         transform.position = Input.mousePosition; // Update the position of the item to follow the mouse
     }
 
@@ -26,17 +23,6 @@ public class InventoryItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEn
     {
         // Handle the end of the drag
         image.raycastTarget = true;
-        transform.SetParent(parentToReturnTo); // Return to the original parent
-    }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        transform.SetParent(parentAfterDrag); // Return to the original parent
     }
 }
